13e960918aeea6cd90a536a3b97448480d79812c
rcaintoy <111341191+rcaintoy@users.noreply.github.com>
2023-02-23 09:21:13 +0800

    Update DULookupService.java

--- src/main/java/com/apc/du/services/DULookupService.java
+++ src/main/java/com/apc/du/services/DULookupService.java
@@ -4,7 +4,7 @@ import com.apc.commons.response.BaseResponse;
 import com.apc.du.exceptions.APException;
 import com.apc.du.exceptions.ServiceDisconnectedException;
 
-public interface DULookupService {
+public interface DULookupService { 
     BaseResponse getDistributionUtility(String province, String city, String barangay, String postalCode) throws ServiceDisconnectedException;
 
     BaseResponse findAll(String du, String province, String city, String postalCode, String barangay) throws APException;
